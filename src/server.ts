cd2afb32f899b3a5eb23a52f6fa47f27ade3718f
marco bruno <marco2000bruno@gmail.com>
2023-12-14 15:18:53 +0100

    esercizio16

--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,43 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import 'express-async-errors';
-import Joi from 'joi';
-import {getAll , getOneById ,create ,updateById , deleteById} from "./controllers/planets.js"
+import { log } from "console";
+import express from "express";
+import "express-async-errors"
+import morgan from "morgan"
+import {getAll, getOneById, create, updateById, deleteById, createImage} from "./controllers/planets.js"
+import multer from "multer"
 
-dotenv.config();
-
-const app = express();
-
-app.use(express.json());
-app.use(morgan('dev'));
-
-const planetChart = Joi.object({
-  id: Joi.number().required(),
-  name: Joi.string().required(),
-});
-
-const planetValidation = (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const { error } = planetChart.validate(req.body);
-  if (error) {
-    return res.status(400).json({ error: error.details[0].message });
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
   }
-  next();
-};
+})
 
-app.get('/api/planets', getAll);
 
-app.get('/api/planets/:id', getOneById);
+const TODO: string = "start writing your Express API server here :)";
 
-app.post('/api/planets', planetValidation, create);
+console.log(TODO);
 
-app.put('/api/planets/:id', planetValidation, updateById );
+const app = express()
+const port = 3000
 
-app.delete('/api/planets/:id', deleteById);
+app.use("/uploads", express.static("uploads"));
 
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
+app.use(morgan("dev"));
 
-const PORT = process.env.PORT || 3000;
+app.get('/api/planets', getAll)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.get('/api/planets/:id', getOneById)
+
+app.post('/api/planets', create)
+
+app.put('/api/planets/:id', updateById)
+
+app.delete('/api/planets/:id', deleteById)
+
+app.post('/api/planets/:id/image', upload.single("image"), createImage)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
